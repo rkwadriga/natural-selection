5e18414c1c543d7f383c1f76c0dfc942db72390e
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-03-19 21:13:07 +0200

    Add docker-compose with web (apache2 + php7.4) and db (mysql) containers.

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
